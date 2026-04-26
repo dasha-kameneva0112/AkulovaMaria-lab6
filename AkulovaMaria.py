9788f035cb34d66c52f51ad746927bc68249787c
dasha-kameneva <dasha-kameneva969@yandex.ru>
2026-04-26 11:11:53 +0300

    init: вынесены TITLE и EXIT_MSG в переменные

--- AkulovaMaria.py
+++ AkulovaMaria.py
@@ -1,4 +1,5 @@
 import math
+
 def rectangle_area(a, b):
     if a <= 0 or b <= 0:
         return "Ошибка: стороны должны быть положительными"
@@ -20,38 +21,36 @@ def circle_area():
             return
         r = d / 2
     result = math.pi * r * r
+    return result
 
 def triangle_area(base, height):
     if base <= 0 or height <= 0:
         print("Ошибка: основание и высота должны быть положительными")
         return
-    result = 0.5 * base * height
-    return result
+    return 0.5 * base * height
+
+TITLE = "=== КАЛЬКУЛЯТОР ПЛОЩАДЕЙ ==="
+EXIT_MSG = "До свидания!"
 
 while True:
-    print("\n=== КАЛЬКУЛЯТОР ПЛОЩАДЕЙ ===")
+    print(f"\n{TITLE}")
     print("1. Площадь прямоугольника")
     print("2. Площадь круга")
     print("3. Площадь треугольника")
     print("0. Выход")
-
     choice = input("Выбери пункт: ")
-
     if choice == "0":
-        print("До свидания!")
+        print(EXIT_MSG)
         break
     elif choice == "1":
-        print("Выбрано: прямоугольник")
         a = float(input("Введите длину: "))
         b = float(input("Введите ширину: "))
         print(f"Площадь прямоугольника: S = {a} * {b} = {rectangle_area(a, b)}")
     elif choice == "2":
-        print("Выбрано: круг")
         print(f"Площадь круга: {circle_area()}")
     elif choice == "3":
-        print("Выбрано: треугольник")
         base = float(input("Введите основание: "))
         height = float(input("Введите высоту: "))
-        print(f"Площадь треульника: {triangle_area(base, height)}")
+        print(f"Площадь треугольника: {triangle_area(base, height)}")
     else:
         print("Неверный ввод!")
